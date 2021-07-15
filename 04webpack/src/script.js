f76f5f12fbb2189285baa789e0afc3ba98a845c3
Nikita <69010558+Kapibarra@users.noreply.github.com>
2021-07-15 23:36:37 +0300

    group section  with cubes

--- 04webpack/src/script.js
+++ 04webpack/src/script.js
@@ -4,31 +4,33 @@ import * as THREE from 'three'
 // Scene
 const scene = new THREE.Scene()
 
-// Red Cube
 
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
+// Group
+const group = new THREE.Group()
+group.position.y = 1
+group.scale.y = 2
+scene.add(group)
 
-// Positioning
+const cube1 = new THREE.Mesh(
+new THREE.BoxGeometry(1,1,1),
+new THREE.MeshBasicMaterial({color: 0xff0000})
+)
 
-// mesh.position.x = 0.7
-// mesh.position.y = -0.6
-// mesh.position.z = 1
-mesh.position.set(0.7, -0.6, 1)
+group.add(cube1)
 
-scene.add(mesh)
-//Scale 
-// mesh.scale.x = 1.5
-// mesh.scale.y = 0.4
-// mesh.scale.z = 0.4
-mesh.scale.set(1.5, 0.4, 0.4)
+const cube2 = new THREE.Mesh(
+    new THREE.BoxGeometry(1,1,1),
+    new THREE.MeshBasicMaterial({color: 0x002Aff})
+    )
+cube2.position.x = 1.3    
+group.add(cube2)
 
-//Rotation 
-mesh.rotation.reorder('YXZ')
-mesh.rotation.x = Math.PI * 0.25
-mesh.rotation.y = Math.PI * 0.25
-
+const cube3 = new THREE.Mesh(
+        new THREE.BoxGeometry(1,1,1),
+        new THREE.MeshBasicMaterial({color: 0x00ff2A})
+        )
+        cube3.position.x = -1.3         
+group.add(cube3)
 // Axis helper
 
 const axesHelper = new THREE.AxesHelper()
@@ -46,7 +48,7 @@ camera.position.z = 3
 // camera.position.x = 1
 scene.add(camera)
 
-camera.lookAt(new THREE.Vector3(0, -1, 0) )
+// camera.lookAt(new THREE.Vector3(0, -1, 0) )
 
 // Renderer
 const canvas = document.querySelector('.webgl')
