e0a4cd0943393f55a8bc98e180978733daa1d919
Nikita <69010558+Kapibarra@users.noreply.github.com>
2021-07-26 22:59:34 +0300

    lesson06 animations

--- 04webpack/src/script.js
+++ 04webpack/src/script.js
@@ -1,60 +1,51 @@
 import './style.css'
 import * as THREE from 'three'
+import gsap from 'gsap'
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
 
 // Scene
 const scene = new THREE.Scene()
 
+// Object
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
 
-// Group
-const group = new THREE.Group()
-group.position.y = 1
-group.scale.y = 2
-scene.add(group)
-
-const cube1 = new THREE.Mesh(
-new THREE.BoxGeometry(1,1,1),
-new THREE.MeshBasicMaterial({color: 0xff0000})
-)
-
-group.add(cube1)
-
-const cube2 = new THREE.Mesh(
-    new THREE.BoxGeometry(1,1,1),
-    new THREE.MeshBasicMaterial({color: 0x002Aff})
-    )
-cube2.position.x = 1.3    
-group.add(cube2)
-
-const cube3 = new THREE.Mesh(
-        new THREE.BoxGeometry(1,1,1),
-        new THREE.MeshBasicMaterial({color: 0x00ff2A})
-        )
-        cube3.position.x = -1.3         
-group.add(cube3)
-// Axis helper
-
-const axesHelper = new THREE.AxesHelper()
-scene.add(axesHelper)
-
-
-// Camera
+// Sizes
 const sizes = {
     width: 800,
     height: 600
 }
+
+// Camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.z = 3
-// camera.position.y = 1
-// camera.position.x = 1
 scene.add(camera)
 
-// camera.lookAt(new THREE.Vector3(0, -1, 0) )
-
 // Renderer
-const canvas = document.querySelector('.webgl')
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
-
 renderer.render(scene, camera)
+
+// CLock
+// const clock = new THREE.Clock()
+gsap.to(mesh.position, { duration:1, delay: 1, x: 2 })
+// animation
+const tick = () => {
+// time
+// const elapsedTime = clock.getElapsedTime()
+
+// update onjects
+// mesh.rotation.x = elapsedTime * Math.PI * 2
+
+// render
+renderer.render(scene, camera)
+window.requestAnimationFrame(tick)
+}
+
+tick()
