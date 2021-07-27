78f4c19def872efecf8c86575036324c0b6d7a73
Nikita <69010558+Kapibarra@users.noreply.github.com>
2021-07-27 13:21:09 +0300

    07 cameras - start

--- 04webpack/src/script.js
+++ 04webpack/src/script.js
@@ -1,18 +1,11 @@
 import './style.css'
 import * as THREE from 'three'
-import gsap from 'gsap'
 
+/**
+ * Base
+ */
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
-
-// Scene
-const scene = new THREE.Scene()
-
-// Object
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
 
 // Sizes
 const sizes = {
@@ -20,9 +13,22 @@ const sizes = {
     height: 600
 }
 
+// Scene
+const scene = new THREE.Scene()
+
+// Object
+const mesh = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1, 5, 5, 5),
+    new THREE.MeshBasicMaterial({ color: 0xff0000 })
+)
+scene.add(mesh)
+
 // Camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-camera.position.z = 3
+camera.position.x = 2
+camera.position.y = 2
+camera.position.z = 2
+camera.lookAt(mesh.position)
 scene.add(camera)
 
 // Renderer
@@ -30,22 +36,22 @@ const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
-renderer.render(scene, camera)
 
-// CLock
-// const clock = new THREE.Clock()
-gsap.to(mesh.position, { duration:1, delay: 1, x: 2 })
-// animation
-const tick = () => {
-// time
-// const elapsedTime = clock.getElapsedTime()
+// Animate
+const clock = new THREE.Clock()
 
-// update onjects
-// mesh.rotation.x = elapsedTime * Math.PI * 2
+const tick = () =>
+{
+    const elapsedTime = clock.getElapsedTime()
 
-// render
-renderer.render(scene, camera)
-window.requestAnimationFrame(tick)
+    // Update objects
+    mesh.rotation.y = elapsedTime;
+
+    // Render
+    renderer.render(scene, camera)
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
 }
 
 tick()
