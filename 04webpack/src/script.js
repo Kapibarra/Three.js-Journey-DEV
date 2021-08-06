2bdfc05350464a9cb073a380f1dbc930302d52d1
Nikita <69010558+Kapibarra@users.noreply.github.com>
2021-08-06 16:52:50 +0300

    init 015

--- 04webpack/src/script.js
+++ 04webpack/src/script.js
@@ -18,81 +18,44 @@ const scene = new THREE.Scene()
 /**
  * Lights
  */
+// Ambient light
 const ambientLight = new THREE.AmbientLight(0xffffff, 0.5)
-scene.add(ambientLight)
 gui.add(ambientLight, 'intensity').min(0).max(1).step(0.001)
+scene.add(ambientLight)
 
-const directionalLight = new THREE.DirectionalLight(0x00fffc, 0.3)
-directionalLight.position.set(1,0.25,0)
-scene.add(directionalLight)
+// Directional light
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5)
+directionalLight.position.set(2, 2, - 1)
 gui.add(directionalLight, 'intensity').min(0).max(1).step(0.001)
+gui.add(directionalLight.position, 'x').min(- 5).max(5).step(0.001)
+gui.add(directionalLight.position, 'y').min(- 5).max(5).step(0.001)
+gui.add(directionalLight.position, 'z').min(- 5).max(5).step(0.001)
+scene.add(directionalLight)
 
+/**
+ * Materials
+ */
+const material = new THREE.MeshStandardMaterial()
+material.roughness = 0.7
+gui.add(material, 'metalness').min(0).max(1).step(0.001)
+gui.add(material, 'roughness').min(0).max(1).step(0.001)
 
-const hemisphereLights = new THREE.HemisphereLight(0xff0000, 0x0000ff, 0.3)
-scene.add(hemisphereLights)
-
-const pointLight = new THREE.PointLight(0xff9000,0.5,10,2)
-pointLight.position.set(1, -0.5,1)
-scene.add(pointLight)
-gui.add(pointLight,'distance').min(1).max(10).step(0.5)
-
-const rectAreaLight = new THREE.RectAreaLight(0x4e00ff,2,1,1)
-rectAreaLight.position.set(- 1.5,0,1.5)
-scene.add(rectAreaLight)
-
-const spotLight = new THREE.SpotLight(0x78ff00, 0.5,10,Math.PI * 0.1, 0.25, 1)
-spotLight.position.set(0,2,3)
-scene.add(spotLight)
-
-
-// Helpers Lights
-const hemisphereLightHelper = new THREE.HemisphereLightHelper(hemisphereLights, 0.2)
-scene.add(hemisphereLightHelper)
-
-const DirectionalLightHelper = new THREE.DirectionalLightHelper(directionalLight, 0.2)
-scene.add(DirectionalLightHelper)
-
-const PointLightHelper = new THREE.PointLightHelper(pointLight, 0.2)
-scene.add(PointLightHelper)
-
-const SpotLightHelper = new THREE.SpotLightHelper(spotLight)
-scene.add(SpotLightHelper)
-window.requestAnimationFrame(() => {
-  SpotLightHelper.update()
-})
 /**
  * Objects
  */
-// Material
-const material = new THREE.MeshStandardMaterial()
-material.roughness = 0.4
-
-// Objects
 const sphere = new THREE.Mesh(
     new THREE.SphereBufferGeometry(0.5, 32, 32),
     material
 )
-sphere.position.x = - 1.5
-
-const cube = new THREE.Mesh(
-    new THREE.BoxBufferGeometry(0.75, 0.75, 0.75),
-    material
-)
-
-const torus = new THREE.Mesh(
-    new THREE.TorusBufferGeometry(0.3, 0.2, 32, 64),
-    material
-)
-torus.position.x = 1.5
 
 const plane = new THREE.Mesh(
     new THREE.PlaneBufferGeometry(5, 5),
     material
 )
 plane.rotation.x = - Math.PI * 0.5
-plane.position.y = - 0.65
+plane.position.y = - 0.5
 
-scene.add(sphere, cube, torus, plane)
+scene.add(sphere, plane)
 
 /**
  * Sizes
@@ -124,7 +87,7 @@ window.addEventListener('resize', () =>
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
 camera.position.x = 1
 camera.position.y = 1
-camera.position.z = 6
+camera.position.z = 2
 scene.add(camera)
 
 // Controls
@@ -148,15 +111,6 @@ const clock = new THREE.Clock()
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
-
-    // Update objects
-    sphere.rotation.y = 0.1 * elapsedTime
-    cube.rotation.y = 0.1 * elapsedTime
-    torus.rotation.y = 0.1 * elapsedTime
-
-    sphere.rotation.x = 0.15 * elapsedTime
-    cube.rotation.x = 0.15 * elapsedTime
-    torus.rotation.x = 0.15 * elapsedTime
 
     // Update controls
     controls.update()
