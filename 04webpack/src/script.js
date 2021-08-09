74c4de9e0963be23a3102200149ab877d402a68a
Nikita <69010558+Kapibarra@users.noreply.github.com>
2021-08-09 17:18:58 +0300

    finished 017

--- 04webpack/src/script.js
+++ 04webpack/src/script.js
@@ -25,10 +25,13 @@ const particlesGeometry = new THREE.BufferGeometry()
 const count = 5000
 
 const positions = new Float32Array(count * 3)
+const colors = new Float32Array(count * 3)
 for(let i = 0; i < count * 3; i++) {
     positions[i] = (Math.random() - 0.5) * 10
+    colors[i] = Math.random()
 }
 particlesGeometry.setAttribute('position', new THREE.BufferAttribute(positions, 3))
+particlesGeometry.setAttribute('color', new THREE.BufferAttribute(colors, 3))
 const particlesMaterial = new THREE.PointsMaterial({
     size: 0.1,
     sizeAttenuation: true,
@@ -36,7 +39,10 @@ const particlesMaterial = new THREE.PointsMaterial({
 particlesMaterial.transparent = true
 particlesMaterial.alphaMap = particleTexture
 // particlesMaterial.alphaText = 0.001
-particlesMaterial.depthTest = false
+// particlesMaterial.depthTest = false
+// particlesMaterial.blending = THREE.AdditiveBlending
+particlesMaterial.depthWrite = false
+particlesMaterial.vertexColors = true
 
 // points_
 
@@ -95,7 +101,16 @@ const clock = new THREE.Clock()
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
+// update particles 
+for(let i = 0; i < count; i++)
+{
+    const i3 = i * 3
 
+    const x = particlesGeometry.attributes.position.array[i3 + 0]
+    particlesGeometry.attributes.position.array[i3 + 1] = Math.sin(elapsedTime * x)
+}
+particlesGeometry.attributes.position.needsUpdate = true
+// particles.rotation.y = elapsedTime * 0.2
     // Update controls
     controls.update()
 
