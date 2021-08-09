a29eb9db8d8d21650a2448257400d40ecbf70ad0
Nikita <69010558+Kapibarra@users.noreply.github.com>
2021-08-09 13:12:56 +0300

    added new light 016

--- 04webpack/src/script.js
+++ 04webpack/src/script.js
@@ -92,7 +92,8 @@ for(let i = 0; i < 50; i++) {
     
 const grave = new THREE.Mesh(graveGeometry, graveMaterial)
 grave.position.set(x,0.3,z)
-grave.rotation.z = (Math.random() - 0.5) * 0.4
+grave.rotation.z = (Math.random() - 0.5) * 0.8
+grave.rotation.y = (Math.random() - 0.5) * 0.8
 graves.add(grave)
 }
 
@@ -109,18 +110,23 @@ scene.add(floor)
  * Lights
  */
 // Ambient light
-const ambientLight = new THREE.AmbientLight('#ffffff', 0.5)
+const ambientLight = new THREE.AmbientLight('#b9d5ff', 0.12)
 gui.add(ambientLight, 'intensity').min(0).max(1).step(0.001)
 scene.add(ambientLight)
 
 // Directional light
-const moonLight = new THREE.DirectionalLight('#ffffff', 0.5)
+const moonLight = new THREE.DirectionalLight('#b9d5ff', 0.12)
 moonLight.position.set(4, 5, - 2)
 gui.add(moonLight, 'intensity').min(0).max(1).step(0.001)
 gui.add(moonLight.position, 'x').min(- 5).max(5).step(0.001)
 gui.add(moonLight.position, 'y').min(- 5).max(5).step(0.001)
 gui.add(moonLight.position, 'z').min(- 5).max(5).step(0.001)
 scene.add(moonLight)
+
+//Door light 
+const doorLight = new THREE.PointLight('#ff7d46' , 1 , 7)
+doorLight.position.set(0, 1.9 ,2.7)
+house.add(doorLight)
 
 /**
  * Sizes
