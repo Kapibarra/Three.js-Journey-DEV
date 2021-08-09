decddbdd50568505581a6c8f08a74f1edd8e18b5
Nikita <69010558+Kapibarra@users.noreply.github.com>
2021-08-09 16:32:59 +0300

    added particles 017

--- 04webpack/src/script.js
+++ 04webpack/src/script.js
@@ -19,15 +19,29 @@ const scene = new THREE.Scene()
  * Textures
  */
 const textureLoader = new THREE.TextureLoader()
+const particleTexture = textureLoader.load('/textures/particles/2.png')
 
-/**
- * Test cube
- */
-const cube = new THREE.Mesh(
-    new THREE.BoxBufferGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial()
-)
-scene.add(cube)
+const particlesGeometry = new THREE.BufferGeometry()
+const count = 5000
+
+const positions = new Float32Array(count * 3)
+for(let i = 0; i < count * 3; i++) {
+    positions[i] = (Math.random() - 0.5) * 10
+}
+particlesGeometry.setAttribute('position', new THREE.BufferAttribute(positions, 3))
+const particlesMaterial = new THREE.PointsMaterial({
+    size: 0.1,
+    sizeAttenuation: true,
+})
+particlesMaterial.transparent = true
+particlesMaterial.alphaMap = particleTexture
+// particlesMaterial.alphaText = 0.001
+particlesMaterial.depthTest = false
+
+// points_
+
+const particles = new THREE.Points(particlesGeometry, particlesMaterial)
+scene.add(particles)
 
 /**
  * Sizes
