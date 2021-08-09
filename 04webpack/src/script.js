78518d2b1ee6496a9ece723ac03bd47d3f875a55
Nikita <69010558+Kapibarra@users.noreply.github.com>
2021-08-09 14:22:37 +0300

    optimization for shadows 016 end

--- 04webpack/src/script.js
+++ 04webpack/src/script.js
@@ -262,6 +262,8 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 // Shadows 
 
 renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap
+
 moonLight.castShadow = true 
 doorLight.castShadow = true;
 ghost1.castShadow = true
@@ -276,6 +278,21 @@ bush4.castShadow = true
 
 floor.receiveShadow = true
 
+doorLight.shadow.mapSize.width = 256
+doorLight.shadow.mapSize.height = 256
+doorLight.shadow.camera.far = 7
+
+ghost1.shadow.mapSize.width = 256
+ghost1.shadow.mapSize.height = 256
+ghost1.shadow.camera.far = 7
+
+ghost2.shadow.mapSize.width = 256
+ghost2.shadow.mapSize.height = 256
+ghost2.shadow.camera.far = 7
+
+ghost3.shadow.mapSize.width = 256
+ghost3.shadow.mapSize.height = 256
+ghost3.shadow.camera.far = 7
 /**
  * Animate
  */
