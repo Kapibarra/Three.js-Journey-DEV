3fbf4df1e74de95906883d17fcb50c3a5e1bd05a
Nikita <69010558+Kapibarra@users.noreply.github.com>
2021-08-09 14:19:45 +0300

    added cast shadows 016

--- 04webpack/src/script.js
+++ 04webpack/src/script.js
@@ -155,11 +155,11 @@ for (let i = 0; i < 50; i++) {
   const radius = 4 + Math.random() * 6;
   const x = Math.sin(angle) * radius;
   const z = Math.cos(angle) * radius;
-
   const grave = new THREE.Mesh(graveGeometry, graveMaterial);
   grave.position.set(x, 0.3, z);
   grave.rotation.z = (Math.random() - 0.5) * 0.8;
   grave.rotation.y = (Math.random() - 0.5) * 0.8;
+  grave.castShadow = true
   graves.add(grave);
 }
 
@@ -203,6 +203,13 @@ const doorLight = new THREE.PointLight("#ff7d46", 1, 7);
 doorLight.position.set(0, 2.15, 2.7);
 house.add(doorLight);
 
+// Ghost light
+const ghost1 = new THREE.PointLight("#ff00ff" , 2, 3)
+scene.add(ghost1)
+const ghost2 = new THREE.PointLight("#00ffff" , 2, 3)
+scene.add(ghost2)
+const ghost3 = new THREE.PointLight("#ffff00" , 2, 3)
+scene.add(ghost3)
 /**
  * Sizes
  */
@@ -252,21 +259,54 @@ const renderer = new THREE.WebGLRenderer({
 });
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+// Shadows 
+
+renderer.shadowMap.enabled = true;
+moonLight.castShadow = true 
+doorLight.castShadow = true;
+ghost1.castShadow = true
+ghost2.castShadow = true
+ghost3.castShadow = true
+
+walls.castShadow = true
+bush1.castShadow = true
+bush2.castShadow = true
+bush3.castShadow = true
+bush4.castShadow = true
+
+floor.receiveShadow = true
 
 /**
  * Animate
  */
+
 const clock = new THREE.Clock();
 
 const tick = () => {
   const elapsedTime = clock.getElapsedTime();
+// Ghosts 
+const ghostAngle = elapsedTime * 0.5;
+ghost1.position.x = Math.cos(ghostAngle) * 4
+ghost1.position.y = Math.sin(ghostAngle) * 4
+ghost1.position.z = Math.sin(elapsedTime * 3)
 
+const ghost2Angle = - elapsedTime * 0.32;
+ghost2.position.x = Math.cos(ghost2Angle) * 5
+ghost2.position.y = Math.sin(ghost2Angle) * 5
+ghost2.position.z = Math.sin(elapsedTime * 4) + Math.sin(elapsedTime * 2.5)
+
+const ghost3Angle = - elapsedTime * 0.18;
+ghost3.position.x = Math.cos(ghost3Angle) * (7 + Math.sin(elapsedTime * 0.32))
+ghost3.position.y = Math.sin(ghost3Angle) * (7 + Math.sin(elapsedTime * 0.5))
+ghost3.position.z = Math.sin(elapsedTime * 4) + Math.sin(elapsedTime * 2.5)
   // Update controls
   controls.update();
 
   // Render
   renderer.render(scene, camera);
   renderer.setClearColor("#262837");
+
+
 
   // Call tick again on the next frame
   window.requestAnimationFrame(tick);
