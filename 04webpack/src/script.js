442eecc8a0c1a1a748fd59d7d2333a9b29cfe9a7
Nikita <69010558+Kapibarra@users.noreply.github.com>
2021-08-09 10:51:32 +0300

    baked shadow on meshmaterial 015

--- 04webpack/src/script.js
+++ 04webpack/src/script.js
@@ -6,6 +6,9 @@ import * as dat from 'dat.gui'
 /**
  * Base
  */
+const textureLoader = new THREE.TextureLoader()
+const bakedShadow = textureLoader.load('/textures/bakedShadow.jpg')
+
 // Debug
 const gui = new dat.GUI()
 
@@ -73,7 +76,7 @@ pointLight.castShadow = true
 pointLight.position.set( -1, 1 , 0)
 scene.add(pointLight)
 const pointLightCameraHelper = new THREE.CameraHelper(pointLight.shadow.camera)
-
+pointLightCameraHelper.visible = false
 scene.add(pointLightCameraHelper)
 /**
  * Materials
@@ -94,7 +97,7 @@ sphere.castShadow = true
 
 const plane = new THREE.Mesh(
     new THREE.PlaneBufferGeometry(5, 5),
-    material
+    new THREE.MeshBasicMaterial({map: bakedShadow})
 )
 plane.rotation.x = - Math.PI * 0.5
 plane.position.y = - 0.5
@@ -151,6 +154,7 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
 renderer.shadowMap.enabled = true
 renderer.shadowMap.type = THREE.PCFSoftShadowMap
+renderer.shadowMap.enabled = false
 
 /**
  * Animate
