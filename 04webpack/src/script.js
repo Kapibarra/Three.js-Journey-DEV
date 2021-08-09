b2e5b95b62eba914ef6373784e1ecd4998f14794
Nikita <69010558+Kapibarra@users.noreply.github.com>
2021-08-09 13:08:34 +0300

    added basic objects 016

--- 04webpack/src/script.js
+++ 04webpack/src/script.js
@@ -23,13 +23,78 @@ const textureLoader = new THREE.TextureLoader()
 /**
  * House
  */
-// Temporary sphere
-const sphere = new THREE.Mesh(
-    new THREE.SphereBufferGeometry(1, 32, 32),
-    new THREE.MeshStandardMaterial({ roughness: 0.7 })
+// group 
+const house = new THREE.Group()
+scene.add(house)
+
+// Walls
+const walls = new THREE.Mesh(
+    new THREE.BoxBufferGeometry(4,2.5,4),
+    new THREE.MeshStandardMaterial({color:'#ac8e82'})
 )
-sphere.position.y = 1
-scene.add(sphere)
+walls.position.y = 2.5 / 2
+house.add(walls)
+// Roof 
+const roof = new THREE.Mesh(
+    new THREE.ConeBufferGeometry(3.5,3,4),
+    new THREE.MeshStandardMaterial({ color: '#b35f45'})
+)
+roof.rotation.y = Math.PI * 0.25
+roof.position.y = 3 + 1 / 2
+house.add(roof)
+
+// door 
+
+const door = new THREE.Mesh(
+    new THREE.PlaneBufferGeometry(2,2),
+    new THREE.MeshStandardMaterial({ color: '#aa7b7b'})
+)
+door.position.y = 1
+door.position.z = 2 + 0.01
+house.add(door)
+
+// bushes 
+
+const bushGeometry = new THREE.SphereBufferGeometry(1, 16,16)
+const bushMaterial = new THREE.MeshStandardMaterial({ color: '#89c854'})
+
+const bush1 = new THREE.Mesh(bushGeometry, bushMaterial)
+bush1.scale.set(0.5,0.5,0.5)
+bush1.position.set(0.8,0.2,2.2)
+
+const bush2 = new THREE.Mesh(bushGeometry, bushMaterial)
+bush2.scale.set(0.25,0.25,0.25)
+bush2.position.set(1.8,0.1,2.1)
+
+const bush3 = new THREE.Mesh(bushGeometry, bushMaterial)
+bush3.scale.set(0.4,0.6,0.4)
+bush3.position.set(-1.8,0.3,2.5)
+
+const bush4 = new THREE.Mesh(bushGeometry, bushMaterial)
+bush4.scale.set(0.2,0.2,0.2)
+bush4.position.set(1.5,0.05,2.7)
+
+house.add(bush1, bush2, bush3, bush4)
+// Graves 
+
+const graves = new THREE.Group()
+scene.add(graves)
+
+const graveGeometry = new THREE.BoxBufferGeometry(0.6,0.8,0.2)
+const graveMaterial = new THREE.MeshStandardMaterial({ color: '#b2b6b1'})
+
+for(let i = 0; i < 50; i++) {
+    const angle = Math.random() * Math.PI * 2
+    const radius = 4 + Math.random() * 6
+    const x = Math.sin(angle) * radius
+    const z = Math.cos(angle) * radius
+
+    
+const grave = new THREE.Mesh(graveGeometry, graveMaterial)
+grave.position.set(x,0.3,z)
+grave.rotation.z = (Math.random() - 0.5) * 0.4
+graves.add(grave)
+}
 
 // Floor
 const floor = new THREE.Mesh(
