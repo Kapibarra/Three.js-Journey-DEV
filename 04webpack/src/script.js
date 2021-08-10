ef225e0e8ab0087906b88f8d461b6e858b16e092
Nikita <69010558+Kapibarra@users.noreply.github.com>
2021-08-10 14:13:20 +0300

    add a spin 018

--- 04webpack/src/script.js
+++ 04webpack/src/script.js
@@ -19,6 +19,10 @@ const scene = new THREE.Scene()
 const parameters = {}
 parameters.count = 100000
 parameters.size = 0.01
+parameters.radius = 5
+parameters.branches = 3
+parameters.spin = 1
+parameters.randomness = 0.2
 
 let geometry = null
 let material = null
@@ -35,9 +39,14 @@ geometry = new THREE.BufferGeometry()
 const position = new Float32Array(parameters.count * 3)
 for(let i = 0; i < parameters.count; i++) {
     const i3 = i * 3
-    position[i3] = (Math.random() - 0.5) * 3
-    position[i3 + 1] = (Math.random() - 0.5) * 3
-    position[i3 + 2] = (Math.random() - 0.5) * 3
+
+    const radius =  Math.random() * parameters.radius
+    const branchAngle = (i % parameters.branches) / parameters.branches * Math.PI * 2
+    const spinAngle = radius * parameters.spin
+
+    position[i3] = Math.cos(branchAngle + spinAngle) * radius
+    position[i3 + 1] = 0
+    position[i3 + 2] = Math.sin(branchAngle + spinAngle) * radius
 }
 geometry.setAttribute('position', new THREE.BufferAttribute(position, 3))
 
@@ -54,6 +63,10 @@ generateGalaxy()
 
 gui.add(parameters,'count').min(100).max(100000).step(100).onFinishChange(generateGalaxy)
 gui.add(parameters,'size').min(0.001).max(0.1).step(0.0001).onFinishChange(generateGalaxy)
+gui.add(parameters,'radius').min(0.01).max(20).step(0.1).onFinishChange(generateGalaxy)
+gui.add(parameters,'branches').min(2).max(20).step(1).onFinishChange(generateGalaxy)
+gui.add(parameters,'spin').min(-5).max(5).step(0.01).onFinishChange(generateGalaxy)
+gui.add(parameters,'randomness').min(0).max(2).step(0.01).onFinishChange(generateGalaxy)
 /**
  * Sizes
  */
