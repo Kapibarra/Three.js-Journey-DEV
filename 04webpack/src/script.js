a7b82b57c167295612543dce6bf81361005adcd3
Nikita <69010558+Kapibarra@users.noreply.github.com>
2021-08-10 18:16:02 +0300

    ball pfall physics 020

--- 04webpack/src/script.js
+++ 04webpack/src/script.js
@@ -31,7 +31,20 @@ const environmentMapTexture = cubeTextureLoader.load([
     '/textures/environmentMaps/0/pz.png',
     '/textures/environmentMaps/0/nz.png'
 ])
+// Physics 
+// World 
+const world = new CANNON.World()
+world.gravity.set(0, -9.82, 0)
 
+// Sphere 
+const sphereShape = new CANNON.Sphere(0.5)
+const sphereBody = new CANNON.Body({
+    mass:1,
+    position: new CANNON.Vec3(0,3,0),
+    shape: sphereShape
+})
+
+world.addBody(sphereBody)
 /**
  * Test sphere
  */
@@ -130,10 +143,17 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
  * Animate
  */
 const clock = new THREE.Clock()
+let oldElapsedTime = 0
 
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
+    const deltaTime = elapsedTime - oldElapsedTime
+    oldElapsedTime = elapsedTime
+    // Update physics world 
+    world.step(1 / 60, deltaTime,3)
+
+sphere.position.copy(sphereBody.position)
 
     // Update controls
     controls.update()
