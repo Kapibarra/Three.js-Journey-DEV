4cf83d8913903c91c8be8a522d8ab52d1f7f4f8d
Nikita <69010558+Kapibarra@users.noreply.github.com>
2021-08-10 17:59:42 +0300

    added cannon js 20l

--- 04webpack/src/script.js
+++ 04webpack/src/script.js
@@ -2,7 +2,7 @@ import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'dat.gui'
-
+import CANNON from 'cannon'
 /**
  * Debug
  */
