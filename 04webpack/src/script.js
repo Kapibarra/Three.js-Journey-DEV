3669d08ed5c7101b119c1d6394d76cfee99dd26c
Nikita <69010558+Kapibarra@users.noreply.github.com>
2021-08-11 13:50:04 +0300

    optimize -20

--- 04webpack/src/script.js
+++ 04webpack/src/script.js
@@ -173,15 +173,18 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 // Utils Sphere
 const objectsToUpdate = [];
 
+const sphereGeometry = new THREE.SphereBufferGeometry(1, 20, 20)
+const sphereMaterial = new THREE.MeshStandardMaterial({
+    metalness: 0.3,
+    roughness: 0.4,
+    envMap: environmentMapTexture,
+})
 const createSphere = (radius, position) => {
   const mesh = new THREE.Mesh(
-    new THREE.SphereBufferGeometry(radius, 20, 20),
-    new THREE.MeshStandardMaterial({
-      metalness: 0.3,
-      roughness: 0.4,
-      envMap: environmentMapTexture,
-    })
+    sphereGeometry,
+    sphereMaterial
   );
+  mesh.scale.set(radius, radius, radius)
   mesh.castShadow = true;
   mesh.position.copy(position);
   scene.add(mesh);
