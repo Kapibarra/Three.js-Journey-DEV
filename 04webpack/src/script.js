4f98703e2ea878a6bac5b33e791699b170a0a56a
Nikita <69010558+Kapibarra@users.noreply.github.com>
2021-08-11 13:14:00 +0300

    added contact material 20

--- 04webpack/src/script.js
+++ 04webpack/src/script.js
@@ -36,15 +36,38 @@ const environmentMapTexture = cubeTextureLoader.load([
 const world = new CANNON.World()
 world.gravity.set(0, -9.82, 0)
 
+// Materials
+const concreteMaterial = new CANNON.Material('concrete')
+const plasticMaterial = new CANNON.Material('plastic')
+
+const concretePlasticContactMaterial = new CANNON.ContactMaterial(
+    concreteMaterial,
+    plasticMaterial,
+    {
+        friction: 0.1,
+        restitution: 0.7
+    }
+)
+world.addContactMaterial(concretePlasticContactMaterial)
+
 // Sphere 
 const sphereShape = new CANNON.Sphere(0.5)
 const sphereBody = new CANNON.Body({
     mass:1,
     position: new CANNON.Vec3(0,3,0),
-    shape: sphereShape
+    shape: sphereShape,
+    material: plasticMaterial
 })
-
 world.addBody(sphereBody)
+// Floor 
+const floorShape = new CANNON.Plane()
+const floorBody = new CANNON.Body()
+floorBody.material = concreteMaterial
+floorBody.mass = 0
+floorBody.addShape(floorShape)
+floorBody.quaternion.setFromAxisAngle(new CANNON.Vec3(-1,0,0), Math.PI * 0.5)
+world.addBody(floorBody)
+
 /**
  * Test sphere
  */
